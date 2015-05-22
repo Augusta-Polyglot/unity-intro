edf8bebd30a8347266fae04909b08145c4c4129d
Justin Basinger <justin.m.basinger@gmail.com>
2015-05-21 20:02:55 -0400

    Session one of Unity Intro

---
+++ Galaxy Intruders/Assets/ClipToCameraBounds.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClipToCameraBounds : MonoBehaviour {
+
+	private float minPos;
+	private float maxPos;
+
+	private SpriteRenderer dude;
+
+	// Use this for initialization
+	void Start () {
+	
+		dude = this.GetComponent<SpriteRenderer>();
+
+		float halfTheDude = dude.bounds.size.x/2;
+
+		minPos = Camera.main.ScreenToWorldPoint(
+			Vector3.zero).x + halfTheDude;
+
+		maxPos = Camera.main.ScreenToWorldPoint(
+			new Vector3(Camera.main.pixelWidth,0)).x
+			- halfTheDude;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	void LateUpdate(){
+		this.transform.position = new Vector2(
+			Mathf.Clamp(this.transform.position.x,
+		            minPos,
+		            maxPos),
+			this.transform.position.y);
+	}
+}
---
+++ Galaxy Intruders/Assets/MoveRightAndLeft.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRightAndLeft : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow) || 
+		   Input.GetKey(KeyCode.RightArrow)){
+
+			float magnitude = speed * Time.deltaTime;
+
+			if(Input.GetKey(KeyCode.LeftArrow)){
+				magnitude = magnitude * -1;
+			}
+
+			this.transform.position = new Vector2(
+				this.transform.position.x + magnitude,
+				this.transform.position.y);
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+}
